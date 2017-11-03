734b43a81081df9874285db3ff47d126ffb1203c
Kushagra Gupta <kushagra.gupta@practo.com>
2017-11-03 14:38:30 +0530

    Initial Commit

---
+++ Placeholder Cards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Placeholder Cards
+//
+//  Created by Kushagra Gupta on 03/11/17.
+//  Copyright © 2017 Kushagra Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
